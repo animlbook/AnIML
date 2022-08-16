9dedbc7abac299fa7e3fad0c17e533a76012c798
Hunter Schafer <hschafer@uw.edu>
2022-08-16 15:44:27 -0700

    Update many lines animation

--- manim_animations/linear_regression/many_lines_anim.py
+++ manim_animations/linear_regression/many_lines_anim.py
@@ -8,11 +8,11 @@ class Animation(LinearScene):
 
         coeffs = [(2, 0.5), (3, 0.1), (1, 0.4), (2.8, -0.02), (3.5, -0.5), (1.5, 0.2)]
 
-        lines = Group()
+        lines = VGroup()
         for w_0, w_1 in coeffs:
             line = self.linear_function(w_0, w_1, line_color=BLUE)
             lines.add(line)
 
         self.add(lines, self.dots)
-        self.play(ShowCreation(lines, run_time=5, lag_ratio=1))
+        self.play(Create(lines, run_time=5, lag_ratio=1))
         self.wait(2)
