ecf2975c62f85fdacab61ce0760b6077ad9a51f5
Hunter Schafer <hschafer@uw.edu>
2022-08-17 14:28:04 -0700

    Update underfit/overfit animation

--- manim_animations/assessing_performance/train_test_anim.py
+++ manim_animations/assessing_performance/train_test_anim.py
@@ -7,49 +7,48 @@ class Animation(BTrainTestScene):
 
         # show optimimum position
         opt_line = DashedLine(
-            np.array([5, self.true_fn.function(5), 0]),
-            np.array([5, 0, 0]),
-            color=COL_BLUE,
+            self.axes.c2p(5, self.true_error(5), 0),
+            self.axes.c2p(5, 0, 0),
+            color=COL_PURPLE,
         )
-        opt_label = BTex(r"p^{*}", color=COL_BLUE)
-        opt_label.move_to(np.array([5.0, -0.5, 0]))
+        opt_label = BMathTex(r"p^{*}", color=COL_PURPLE)
+        opt_label.next_to(opt_line, DOWN)
         opt_grp = VGroup(opt_line, opt_label)
-        opt_grp.shift(self.centershift)
-        self.play(ShowCreation(opt_line))
+
+        self.play(Create(opt_line))
         self.play(Write(opt_label))
 
         underfit_rect = Polygon(
-            np.array([0.0, self.y_max, 0]),
-            np.array([3.5, self.y_max, 0]),
-            np.array([3.5, self.y_min, 0]),
-            np.array([0.0, self.y_min, 0]),
+            self.axes.c2p(1.0, self.y_max, 0),
+            self.axes.c2p(3.5, self.y_max, 0),
+            self.axes.c2p(3.5, self.y_min, 0),
+            self.axes.c2p(1.0, self.y_min, 0),
             stroke_opacity=0.0,
             fill_color=COL_RED,
             fill_opacity=0.1,
         )
 
-        underfit_rect.shift(self.centershift)
-        underfit_text = BText(r"Underfit \\ Model", color=COL_BLACK)
+        underfit_text = BTex(r"Underfit \\ Model", color=COL_BLACK)
         underfit_text.scale(0.75)
-        underfit_text.move_to(self.axes.c2p(1.5, 1, 0))
+        underfit_text.next_to(underfit_rect, UP)
 
         self.play(FadeIn(underfit_rect))
         self.play(Write(underfit_text))
 
         overfit_rect = Polygon(
-            np.array([7.5, self.y_max, 0]),
-            np.array([self.x_max, self.y_max, 0]),
-            np.array([self.x_max, self.y_min, 0]),
-            np.array([7.5, self.y_min, 0]),
+            self.axes.c2p(7.5, self.y_max, 0),
+            self.axes.c2p(self.x_max, self.y_max, 0),
+            self.axes.c2p(self.x_max, self.y_min, 0),
+            self.axes.c2p(7.5, self.y_min, 0),
             stroke_opacity=0.0,
             fill_color=COL_RED,
             fill_opacity=0.1,
         )
 
-        overfit_rect.shift(self.centershift)
-        overfit_text = BText(r"Overfit \\ Model", color=COL_BLACK)
+        overfit_text = BTex(r"Overfit \\ Model", color=COL_BLACK)
         overfit_text.scale(0.75)
         overfit_text.move_to(self.axes.c2p(11, 3, 0))
+        overfit_text.next_to(overfit_rect, UP)
 
         self.play(FadeIn(overfit_rect))
         self.play(Write(overfit_text))
--- manim_animations/assessing_performance/train_test_scene.py
+++ manim_animations/assessing_performance/train_test_scene.py
@@ -1,35 +1,45 @@
 from manim_config import *
-from config import *
 
+from ap_utils import *
 
-def true_error(x):
-    mid = 5.0
-    slope = 0.3 if x < mid else 0.1
-    return slope * (x - mid) ** 2 + 2
+LABEL_BUFF = 0.1
 
-
-def train_error(x):
-    return 10 * np.exp(-x / 2.0 - 0.25) + 1
-
+GRAPH_CONFIG = {
+    "X_MIN": 1,
+    "X_MAX": 12,
+    "Y_MIN": 0,
+    "Y_MAX": 6,
+}
 
 class BTrainTestScene(BScene):
+    @staticmethod
+    def true_error(x):
+        mid = 5.0
+        slope = 0.3 if x < mid else 0.1
+        print("TRUE", x, slope * (x - mid) ** 2 + 2)
+        return slope * (x - mid) ** 2 + 2
+
+    @staticmethod
+    def train_error(x):
+        print("TRAIN", x, 10 * np.exp(-x / 2.0 - 0.25) + 1)
+        return 10 * np.exp(-x / 2.0 - 0.25) + 1
+
     def set_axes_labels(self):
-        axeslabels = []
         y_label = BText("Error", color=GRAY)
-        y_label.scale(0.75)
-        y_label.move_to(self.axes.c2p(self.x_min, self.y_max, 0) + UP * 0.25)
+        y_label.scale(0.6)
+        y_label.next_to(self.axes, LABEL_BUFF * (UP + LEFT), aligned_edge=UP, buff=1)
+        # y_label.move_to(self.axes.c2p(self.x_min, self.y_max, 0) + UP * 0.25)
 
-        x_label_low = BText(r"Low Model \\ Complexity", color=GRAY)
-        x_label_low.scale(0.75)
-        x_label_low.move_to(
-            self.axes.c2p(self.x_min, self.y_min, 0) + DOWN * 0.5 + RIGHT
-        )
+        x_label_low = BTex(r"Low Model \\ Complexity", color=GRAY)
+        x_label_low.scale(0.6)
+        x_label_low.next_to(self.axes, LABEL_BUFF * (DOWN + LEFT), aligned_edge=LEFT, buff=1)
+        #x_label_low.move_to(
+        #    self.axes.c2p(self.x_min, self.y_min, 0) + DOWN * 0.5 + RIGHT
+        #)
 
-        x_label_high = BText(r"High Model \\ Complexity", color=GRAY)
-        x_label_high.scale(0.75)
-        x_label_high.move_to(
-            self.axes.c2p(self.x_max, self.y_min, 0) + DOWN * 0.5 + LEFT
-        )
+        x_label_high = BTex(r"High Model \\ Complexity", color=GRAY)
+        x_label_high.scale(0.6)
+        x_label_high.next_to(self.axes, LABEL_BUFF * (DOWN + RIGHT), aligned_edge=RIGHT, buff=1)
 
         self.axes_labels = [y_label, x_label_low, x_label_high]
 
@@ -41,38 +51,29 @@ class BTrainTestScene(BScene):
         self.y_min = GRAPH_CONFIG["Y_MIN"]
         self.y_max = GRAPH_CONFIG["Y_MAX"]
 
-        self.set_axes_labels()
+        self.axes.scale(0.7)
+        self.axes.center()
 
     def setup_scene(self):
-        self.true_fn = FunctionOffGraph(
-            x_min=self.x_min,
-            x_max=self.x_max,
-            y_min=self.y_min,
-            y_max=self.y_max,
-            function=true_error,
-            color=COL_GOLD,
-        )
-        self.true_flabel = BText(r"True \\ Error", color=COL_GOLD)
+        self.setup_axes()
+        self.set_axes_labels()
+
+        self.true_fn = self.axes.plot(self.true_error, x_range=(self.x_min, self.x_max), color=COL_GOLD)
+
+        self.true_flabel = BTex(r"True \\ Error", color=COL_GOLD)
         self.true_flabel.scale(0.75)
 
         # move to the right of the axes
-        ypos = max(self.y_min, min(self.y_max, self.true_fn.function(self.x_max)))
+        ypos = max(self.y_min, min(self.y_max, self.true_error(self.x_max)))
         self.true_flabel.move_to(self.axes.c2p(self.x_max, ypos, 0) + RIGHT * 0.75)
 
-        self.train_fn = FunctionOffGraph(
-            x_min=self.x_min,
-            x_max=self.x_max,
-            y_min=self.y_min,
-            y_max=self.y_max,
-            function=train_error,
-            color=COL_BLUE,
-        )
+        self.train_fn = self.axes.plot(self.train_error, x_range=(self.x_min, self.x_max), color=COL_BLUE)
 
-        self.train_flabel = BText(r"Train \\ Error", color=COL_BLUE)
+        self.train_flabel = BTex(r"Train \\ Error", color=COL_BLUE)
         self.train_flabel.scale(0.75)
 
         # move to the right of the axes
-        ypos = max(self.y_min, min(self.y_max, self.train_fn.function(self.x_max)))
+        ypos = max(self.y_min, min(self.y_max, self.train_error(self.x_max)))
         self.train_flabel.move_to(self.axes.c2p(self.x_max, ypos, 0) + RIGHT * 0.75)
 
         self.axes_and_fn_label = VGroup(
@@ -83,11 +84,11 @@ class BTrainTestScene(BScene):
             self.train_flabel,
             *self.axes_labels
         )
-        self.centershift = -self.axes_and_fn_label.get_center()
-        self.axes_and_fn_label.move_to((0, 0, 0))
+        #self.centershift = -self.axes_and_fn_label.get_center()
+        #self.axes_and_fn_label.move_to((0, 0, 0))
 
-        self.play(ShowCreation(self.axes), Write(VGroup(*self.axes_labels)))
-        self.play(ShowCreation(self.true_fn))
+        self.play(Create(self.axes), Write(VGroup(*self.axes_labels)))
+        self.play(Create(self.true_fn))
         self.play(Write(self.true_flabel))
-        self.play(ShowCreation(self.train_fn))
+        self.play(Create(self.train_fn))
         self.play(Write(self.train_flabel))
