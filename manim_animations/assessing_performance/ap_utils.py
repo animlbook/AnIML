719914c55eba6fbb5d7f903ed0c27ab7a7554676
Hunter Schafer <hschafer@uw.edu>
2022-08-18 19:01:57 -0700

    Refactor ap_utils

--- manim_animations/assessing_performance/ap_utils.py
+++ manim_animations/assessing_performance/ap_utils.py
@@ -1,3 +1,8 @@
+# Manim config has a re-definition of Union
+import typing
+from typing import Sequence, Union
+
+import numpy as np
 from manim_config import *
 
 GRAPH_CONFIG = {
@@ -7,8 +12,13 @@ GRAPH_CONFIG = {
     "Y_MAX": 6,
 }
 
+X_RANGE = (0, 12)
+Y_RANGE = (0, 6)
+
+SegmentedParametricFunction = ParametricFunction
+
 class BoundedAxes(Axes):
-    def plot_bounded(self, function, x_range=None, y_range=None, use_vectorized=None, **kwargs):
+    def plot_bounded(self, function, x_range=None, y_range=None, use_vectorized=None, **kwargs) -> SegmentedParametricFunction:
         """
         Same as plot, but hides regions with values outside the y-range
         """
@@ -55,8 +65,129 @@ class BoundedAxes(Axes):
         # Want to also return the "full" function for usability
         full_function = self.plot(function, x_range=x_range, use_vectorized=use_vectorized, **kwargs)
 
-        return full_function, segments
+        full_function.segments = segments
 
+        return full_function
+
+
+
+def polynomial_expand(x: typing.Union[float, Sequence[float]] , deg: int) -> np.ndarray:
+    if np.isscalar(x):
+        features = np.zeros((1, deg + 1))
+        for i in range(deg + 1):
+            features[0, i] = pow(x, i)
+        return features
+    else:  # Is list of xs
+        n = x.shape[0]
+        new_X = np.empty((n, deg + 1))
+        for i in range(n):
+            new_X[i] = polynomial_expand(x[i], deg)[0, :]  # Recurse one level to expand single x
+        return new_X
+
+
+def predict(x, coeffs):
+    x = polynomial_expand(x, len(coeffs) - 1)
+    result = x @ coeffs
+    return result
+
+
+def true_function(x):
+    dim = 6  # Includes intercept
+    true_coeffs = np.array([
+        -9.77643998e+00,
+        1.77393973e+01,
+        -7.34515366e+00,
+        1.27881487e+00,
+        -9.94748672e-02,
+        2.85635347e-03
+    ]).reshape((dim, 1))
+    return predict(x, true_coeffs)
+
+
+def generate_train_data(x_range, n=25, seed=100394, clip_y_range=None):
+    """
+    Generates n data points on the range x_range using given seed.
+
+    If clip_y_range is specified, will clip any examples outside of the range
+    """
+    np.random.seed(seed)
+
+    x_min, x_max = x_range
+    Xs = np.random.uniform(x_min, x_max, n).reshape((n, 1))
+    ys = true_function(Xs) + np.random.normal(0, 0.6, size=(n, 1))
+
+    if clip_y_range:
+        y_min, y_max = clip_y_range
+        valid_mask = (ys >= y_min) & (ys <= y_max)
+        Xs = Xs[valid_mask]
+        ys = ys[valid_mask]
+
+    return Xs, ys
+
+def train(X_train, y_train, deg):
+    assert y_train.shape[1] == 1
+
+    X_train = polynomial_expand(X_train, deg)
+    w_hat = np.matmul(np.linalg.inv(np.matmul(X_train.T, X_train)), np.matmul(X_train.T, y_train))
+
+    return w_hat
+
+
+# Manim code
+def make_bounded_axes(x_range, y_range, axes_labels=None):
+    axes = BoundedAxes(
+        x_range=x_range,
+        y_range=y_range,
+        axis_config={"include_tip": False, "include_ticks": False, "color": GREY_C},
+    )
+
+    if axes_labels:
+        x_label_text, y_label_text = axes_labels
+
+        axes_labels_grp = VGroup()
+
+        if x_label_text:
+            x_label = axes.get_x_axis_label(x_label_text, edge=DOWN).shift(0.5 * DOWN).set_color(GRAY_C)
+            axes_labels_grp.add(x_label)
+
+        if y_label_text:
+            y_label = axes.get_y_axis_label(y_label_text, edge=LEFT, direction=LEFT, buff=0.4).set_color(GRAY_C)
+            axes_labels_grp.add(y_label)
+
+        return axes, axes_labels_grp
+    else:
+        return axes
+
+
+def get_dots_for_data(axes, Xs, ys, x_range, y_range, radius=DEFAULT_DOT_RADIUS):
+    x_min, x_max = x_range
+    y_min, y_max = y_range
+
+    # Make mobjects for points
+    dots = VGroup()
+    for x, y in zip(Xs[:, 0], ys[:, 0]):
+        if x > x_min and x < x_max and y > y_min and y < y_max:
+            point = axes.coords_to_point(x, y, 0)
+            dot = Dot(point, color=COL_BLACK, radius=radius)
+            dots.add(dot)
+
+    return dots
+
+
+def train_and_plot_function(axes, X_train, y_train, deg, x_range, y_range, color, **kwargs):
+    assert y_train.shape[1] == 1
+    assert y_train.shape[0] == len(X_train)
+
+    w_hat = train(X_train, y_train, deg)
+
+    def f(x):
+        return predict(x, w_hat)[0, 0]
+
+    return axes.plot_bounded(f, x_range=x_range, y_range=y_range,
+        color=color, **kwargs)
+
+
+# Old stuff
 
 def simple_poly_regression_true_data():
     dim = 5
@@ -68,8 +199,6 @@ def simple_poly_regression_true_data():
 
 def simple_poly_regression_get_data(scale=1.0, seed=100394, N=25):
     dim, XTRUE, YTRUE, config = simple_poly_regression_true_data()
-
-    b = beta(H(XTRUE, dim), YTRUE)
 
     np.random.seed(seed)
     XS = np.random.uniform(config["X_MIN"], config["X_MAX"], N)
--- manim_animations/assessing_performance/simple_poly_regression_anim.py
+++ manim_animations/assessing_performance/simple_poly_regression_anim.py
@@ -2,6 +2,7 @@ from manim_config import *
 
 from ap_utils import *
 
+Y_RANGE = (0, 8.5)
 
 class Animation(BScene):
     def construct(self):
@@ -10,39 +11,43 @@ class Animation(BScene):
             (3, COL_GOLD),
             (5, COL_PURPLE),
             (7, COL_BLUE),
-            #(8, COL_GREEN),
+            (9, COL_GREEN)
         ]
 
-        xs, ys, config = simple_poly_regression_get_data(seed=100399, N=10)
-        axes, dots = axes_and_data(xs, ys, config)
+        # Generate axes
+        axes, axes_labels = make_bounded_axes(x_range=X_RANGE, y_range=Y_RANGE, axes_labels=("x", "y"))
+        plot = VGroup(axes, axes_labels)
+        plot.center().shift(LEFT).scale(0.8)
 
-        # Position the plot
-        plot = VGroup(axes, dots)
-        plot.center().shift(LEFT)
+        # Generate data and dots
+        Xs, ys = generate_train_data(x_range=X_RANGE, n=10, seed=100399)
+        dots = get_dots_for_data(axes, Xs, ys, x_range=X_RANGE, y_range=Y_RANGE)
 
         # Make all of the individual degree plots
         fns = []
         labels = []
 
-        for deg, col in self.deg_col:
-            fn, segments = degfungraph(axes, xs, ys, deg, col, config)
-            fns.append(segments)
+        for deg, color in self.deg_col:
+            f_hat = train_and_plot_function(axes, Xs, ys, deg, x_range=X_RANGE, y_range=Y_RANGE, color=color)
+            fns.append(f_hat)
 
-            l = BTex(f"p = {deg}", color=col)
+            l = BTex(f"p = {deg}", color=color)
             if len(labels) > 0:
                 l.next_to(labels[-1], DOWN / 2, aligned_edge=LEFT)
 
             labels.append(l)
 
-        fns_group = VGroup(*fns)
         labels_grp = VGroup(*labels)
         labels_grp.next_to(plot, RIGHT)
 
-        # Add dots to foreground to make sure they are displayed above functions
+        # Draw axes
+        self.play(Create(plot))
+
+        # Draw dots and then add them to foreground to make sure they are displayed above functions
+        self.play(Create(dots))
         self.add_foreground_mobject(dots)
 
-        # Animate in
-        self.play(Create(plot))
+        # Animate in lines
         for f, l in zip(fns, labels):
             self.play(Write(l))
-            self.play(Create(f), run_time=1.5)
+            self.play(Create(f.segments), run_time=1.5)
